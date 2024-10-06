2d09a1edfdb04d9cf5ef44d5266f6fd059f7eeff
CI Pipeline <cipipeline@example.com>
2024-10-06 18:27:36 -0300

    feat: script test

--- changelog.sh
+++ changelog.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-tag=$(cat tag.json | grep version | grep -Eo "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+")
+version=$(cat version.json | grep version | grep -Eo "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+")
 
-echo "Estou dentro do repositório?"
-ls -la
-sleep 3
+for THIS_TAG in "$version"; do
 
-echo "Existe alguma tag?"
-git tag -l $tag | tac
-sleep 5
-
-for THIS_TAG in "$tag"; do
-    git tag -l "$THIS_TAG"
+    git tag -l "$THIS_TAG" | tac
 
     if [ $(git tag -l "$THIS_TAG") ]; then
         echo "Tag $THIS_TAG já existe. Adicionando alterações no CHANGELOG.md ..."
+        chmod +x note-releases.sh
+        ./note-releases.sh > CHANGELOG.md
+        git add CHANGELOG.md 
+        git commit -m "docs(CHANGELOG): update release notes"
+        git push origin HEAD:main
     else
         echo "Tag $THIS_TAG não existe. Criando tag e adicionando notas de alterações no CHANGELOG.md ..."
+        git tag "$THIS_TAG"
+        git push  --tags https://github.com/leoviana00/config-repo-base-autochangelog.git HEAD:main
+        chmod +x note-releases.sh
+        ./note-releases.sh > CHANGELOG.md
+        git add CHANGELOG.md 
+        git commit -m "docs(CHANGELOG): update release notes"
+        git push origin HEAD:main
     fi
-
 done
