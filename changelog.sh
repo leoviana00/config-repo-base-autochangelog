b32ed122e6c1a2976dcc1e68c5cfcdf34ae5049b
CI Pipeline <cipipeline@example.com>
2024-10-06 18:13:38 -0300

    feat: mudando a logica do script

--- changelog.sh
+++ changelog.sh
@@ -4,23 +4,10 @@ version=$(cat version.json | grep version | grep -Eo "[[:digit:]]+\.[[:digit:]]+
 
 for THIS_TAG in "$version"; do
 
-    git tag -l "$THIS_TAG" | tac
-
     if [ $(git tag -l "$THIS_TAG") ]; then
         echo "Tag $THIS_TAG já existe. Adicionando alterações no CHANGELOG.md ..."
-        chmod +x note-releases.sh
-        ./note-releases.sh > CHANGELOG.md
-        git add CHANGELOG.md 
-        git commit -m "docs(CHANGELOG): update release notes"
-        git push origin HEAD:main
     else
         echo "Tag $THIS_TAG não existe. Criando tag e adicionando notas de alterações no CHANGELOG.md ..."
-        git tag "$THIS_TAG"
-        git push  --tags https://github.com/leoviana00/config-repo-base-autochangelog.git HEAD:main
-        chmod +x note-releases.sh
-        ./note-releases.sh > CHANGELOG.md
-        git add CHANGELOG.md 
-        git commit -m "docs(CHANGELOG): update release notes"
-        git push origin HEAD:main
     fi
+
 done
