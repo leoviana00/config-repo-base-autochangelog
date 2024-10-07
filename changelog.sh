10282daab04114636968201f9d39288c94bc6d16
CI Pipeline <cipipeline@example.com>
2024-10-07 10:02:52 -0300

    feat: testando verificacao de tags

--- changelog.sh
+++ changelog.sh
@@ -1,11 +1,10 @@
+#!/bin/bash
+
 version=$(cat version.json | grep version | grep -Eo "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+")
 
-for THIS_TAG in "$version"; do
+if [ $(git tag -l "$version") ]; then
+    echo "Tag $version já existe. Adicionando alterações no CHANGELOG.md ..."
+else
+    echo "Tag $version não existe. Criando tag e adicionando notas de alterações no CHANGELOG.md ..."
+fi
 
-    if [ $(git tag -l "$THIS_TAG") ]; then
-        echo "Tag $THIS_TAG já existe. Adicionando alterações no CHANGELOG.md ..."
-    else
-        echo "Tag $THIS_TAG não existe. Criando tag e adicionando notas de alterações no CHANGELOG.md ..."
-    fi
-
-done
